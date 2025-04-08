72dfa9fbd055bcd015d235fa8a5bee28e7f92f1e
yurelima016 <yurelimasouza@gmail.com>
2025-04-07 23:43:41 -0300

    refactored code

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const footerLink = document.querySelector('#pageCreator');
+document.addEventListener('DOMContentLoaded', () => {
+    const footerLink = document.querySelector('#pageCreator');
+    const addButton = document.querySelector('#addTaskButton');
+    const tasksContainer = document.querySelector('#taskContainer');
 
-footerLink.addEventListener('click', () => {
-    window.location.href = 'https://github.com/yurelima016';
-});
-
-const addButton = document.querySelector('#addTaskButton');
-const tasksContainer = document.querySelector('#taskContainer');
-
-addButton.addEventListener('click', () => {
-    const task = document.createElement('div');
-    task.classList.add('taskCard');
-    tasksContainer.appendChild(task);
+    footerLink.addEventListener('click', () => {
+        window.location.href = 'https://github.com/yurelima016';
+    });
+    
+    addButton.addEventListener('click', () => {
+        const task = document.createElement('div');
+        task.classList.add('taskCard');
+        tasksContainer.appendChild(task);
+    });
 });
