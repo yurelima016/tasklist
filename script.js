fa8de958ba421e7a1a9197e1bcba735fccd159b2
yurelima016 <yurelimasouza@gmail.com>
2025-04-07 23:18:41 -0300

    update files

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let footerLink = document.querySelector('#pageCreator');
+const footerLink = document.querySelector('#pageCreator');
 
 footerLink.addEventListener('click', () => {
     window.location.href = 'https://github.com/yurelima016';
+});
+
+const addButton = document.querySelector('#addTaskButton');
+const tasksContainer = document.querySelector('#taskContainer');
+
+addButton.addEventListener('click', () => {
+    const task = document.createElement('div');
+    task.classList.add('taskCard');
+    tasksContainer.appendChild(task);
 });
