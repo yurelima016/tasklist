f5b625db043c69fc47dd10f68b668fcb6e925f61
yurelima016 <yurelimasouza@gmail.com>
2025-04-15 01:24:17 -0300

    update project

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const footerLink = document.querySelector('#pageCreator');
-    const addButton = document.querySelector('#addTaskButton');
-    const tasksContainer = document.querySelector('#taskContainer');
-    const taskDescription = document.querySelector('#description');
+    const optionsButton = document.querySelector('#optionsButton');
+    const optionsIcon = document.querySelector('#optionsIcon');
+    const floatingButtons = document.querySelector('.floating-buttons');
+    const credits = document.querySelector('#credits');
+    const taskContainer = document.querySelector('#taskContainer');
+    const newTask = document.querySelector('#new-task');
 
-    footerLink.addEventListener('click', () => {
-        window.location.href = 'https://github.com/yurelima016';
-    });
-    
-    addButton.addEventListener('click', () => {
-        const description = taskDescription.value.trim();
-
-        if (description === '') {
-            alert('Please enter a task description.');
-            return;
-        }
+    newTask.addEventListener('click', (event) => {
 
         const task = document.createElement('div');
         task.classList.add('taskCard');
-        task.textContent = description;
-        tasksContainer.appendChild(task);
 
-        taskDescription.value = '';        
+        const titleTask = document.createElement('h2');
+        titleTask.classList.add('titleTask');
+
+        const taskPriority = document.createElement('span');
+        taskPriority.classList.add('badge');
+        taskPriority.classList.add('text-bg-success');
+        taskPriority.classList.add('taskPriority');
+
+        const taskDescription = document.createElement('p');
+        taskDescription.classList.add('taskDescription');
+
+        
+        taskContainer.appendChild(task);
+        task.appendChild(titleTask);
+        task.appendChild(taskPriority);
+        task.appendChild(taskDescription);
+
+        console.log('Funcionou!');
     });
+
+    optionsButton.addEventListener('click', () => {
+        // Alterna o ícone do botão principal
+        if (optionsIcon.classList.contains('bi-list')) {
+            optionsIcon.classList.remove('bi-list');
+            optionsIcon.classList.add('bi-x-lg');
+            floatingButtons.classList.add('show');
+        } else {
+            optionsIcon.classList.remove('bi-x-lg');
+            optionsIcon.classList.add('bi-list');
+            floatingButtons.classList.remove('show');
+        }
+        
+    });
+
+    credits.addEventListener('click', () => {
+        window.location.href = 'https://github.com/yurelima016';
+    })
 });
